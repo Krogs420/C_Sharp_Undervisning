7fe806db1c0f2764a08f322e82a0dbfa9469dc29
Kristian Kryger <kristianlaukryger@gmail.com>
2023-09-27 15:03:15 +0200

    Projekt start, plus file reader der læser csv filer

---
+++ app/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+public class CSVReader
+{
+    static void Main(string[] args)
+    {
+        string filePath1 = "setup.csv";
+        string filePath2 = "teams.csv";
+
+        Console.WriteLine("Reading setup.csv:");
+        ReadCsvFile(filePath1);
+
+        Console.WriteLine("\nReading teams.csv:");
+        ReadCsvFile(filePath2);
+    }
+
+    static void ReadCsvFile(string filePath)
+    {
+        try
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                var records = csv.GetRecords<dynamic>();
+
+                foreach (var record in records)
+                {
+                    foreach (var property in record)
+                    {
+                        Console.Write(property.Value + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
